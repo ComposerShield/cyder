9deef81aeacbfd495238c6050f432b3d8952f00b
Adam Shield <adam@antarestech.com>
2025-06-29 09:29:47 +0200

    Add a main file

---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_core/juce_core.h>
+#include <juce_graphics/juce_graphics.h>
+
+
+
+/**
+ * @brief   Entry point of the program.
+ * @param   argc  Number of command‐line arguments.
+ * @param   argv  Array of C‐strings holding the arguments.
+ * @return  Exit status (0 on success, non-zero on error).
+ */
+int main()
+{
+//    AudioPluginFormatManager formatManager;
+//    KnownPluginList knownPluginList;
+//    PluginGraph pluginGraph(formatManager, knownPluginList);
+    
+    return 0;
+}
