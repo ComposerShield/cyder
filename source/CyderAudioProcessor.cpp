219845af0c140263348681d6048df7d90c50ccdb
Adam Shield <adam@antarestech.com>
2025-06-29 15:24:57 +0200

    Basic implementation of processor and editor

--- source/CyderAudioProcessor.cpp
+++ source/CyderAudioProcessor.cpp
@@ -1,4 +1,114 @@
-
 #include "CyderAudioProcessor.hpp"
 
+#include "CyderAudioProcessorEditor.hpp"
+
 //==============================================================================
+CyderAudioProcessor::CyderAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+    : juce::AudioProcessor (juce::AudioProcessor::BusesProperties()
+ #if ! JucePlugin_IsMidiEffect
+ #if ! JucePlugin_IsSynth
+        .withInput ("Input", juce::AudioChannelSet::stereo(), true)
+ #endif
+        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+ #endif
+     )
+#endif
+{
+}
+
+CyderAudioProcessor::~CyderAudioProcessor()
+{
+}
+
+void CyderAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+}
+
+void CyderAudioProcessor::releaseResources()
+{
+}
+
+bool CyderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+    return true;
+}
+
+void CyderAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    buffer.clear();
+}
+
+juce::AudioProcessorEditor* CyderAudioProcessor::createEditor()
+{
+    return new CyderAudioProcessorEditor (*this);
+}
+
+bool CyderAudioProcessor::hasEditor() const
+{
+    return true;
+}
+
+const juce::String CyderAudioProcessor::getName() const
+{
+    return "Cyder";
+}
+
+bool CyderAudioProcessor::acceptsMidi() const
+{
+    return false;
+}
+
+bool CyderAudioProcessor::producesMidi() const
+{
+    return false;
+}
+
+bool CyderAudioProcessor::isMidiEffect() const
+{
+    return false;
+}
+
+double CyderAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int CyderAudioProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int CyderAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void CyderAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String CyderAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void CyderAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+void CyderAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+}
+
+void CyderAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+}
+
+//==============================================================================
+// This creates new instances of the plugin
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new CyderAudioProcessor();
+}
--- source/CyderAudioProcessorEditor.cpp
+++ source/CyderAudioProcessorEditor.cpp
@@ -1,4 +1,29 @@
 
 #include "CyderAudioProcessorEditor.hpp"
 
+#include "CyderAudioProcessor.hpp"
+
 //==============================================================================
+
+CyderAudioProcessorEditor::CyderAudioProcessorEditor(CyderAudioProcessor& p)
+: juce::AudioProcessorEditor(p)
+, processor (p)
+{
+    setSize (400, 300);
+}
+
+CyderAudioProcessorEditor::~CyderAudioProcessorEditor()
+{
+}
+
+void CyderAudioProcessorEditor::paint(juce::Graphics& g)
+{
+    g.fillAll(getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void CyderAudioProcessorEditor::resized()
+{
+
+}
+
+
