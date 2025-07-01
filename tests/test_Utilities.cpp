31fff323a47ce0c4029c70930eb520f6395a9fc8
Adam Shield <adam@antarestech.com>
2025-07-01 11:44:42 +0200

    More unit tests

--- tests/test_Utilities.cpp
+++ tests/test_Utilities.cpp
@@ -44,3 +44,82 @@ TEST(UtilitiesFindPluginDescription, ThrowsWhenPluginNotFound)
     }
     , std::runtime_error);
 }
+
+TEST(UtilitiesCreateInstance, ReturnsInstanceForValidDescription)
+{
+    juce::AudioPluginFormatManager formatManager;
+    formatManager.addDefaultFormats();
+
+    juce::File currentFile(__FILE__);
+    juce::File pluginFile = currentFile.getSiblingFile("../Cyder.vst3");
+
+    ASSERT_TRUE(pluginFile.exists());
+
+    juce::PluginDescription description;
+    EXPECT_NO_THROW(
+    {
+        description = Utilities::findPluginDescription(pluginFile, formatManager);
+    });
+    
+    std::unique_ptr<juce::AudioPluginInstance> instance;
+    EXPECT_NO_THROW(
+    {
+        instance = Utilities::createInstance(description, formatManager, 44100.0, 512);
+    });
+
+    ASSERT_NE(instance, nullptr);
+    EXPECT_EQ(instance->getName(), description.name);
+}
+
+TEST(UtilitiesCreateInstance, ThrowsWhenInvalidDescription)
+{
+    juce::AudioPluginFormatManager formatManager;
+    formatManager.addDefaultFormats();
+
+    juce::PluginDescription invalidDesc;
+    invalidDesc.fileOrIdentifier = "/path/to/nonexistent.vst3";
+    invalidDesc.pluginFormatName = "VST3";
+    invalidDesc.name = "NonexistentPlugin";
+    invalidDesc.version = "1.0.0";
+
+    EXPECT_THROW(
+    {
+        (void)Utilities::createInstance(invalidDesc, formatManager, 44100.0, 512);
+    }
+    , std::runtime_error);
+}
+
+TEST(UtilitiesCopyPluginToTempWithHash, CopiesFileSuccessfully)
+{
+    // Create mock vst3 (which is a directory/bundle)
+    auto tempSource = juce::File::createTempFile("testPlugin.vst3");
+    tempSource.createDirectory();
+    
+    // Create mock binary to stick inside the bundle, give us something within bundle to copy
+    auto mockBinaryFile = tempSource.getChildFile("Contents").getChildFile("MacOS").getChildFile("testPlugin");
+    [[maybe_unused]] auto result = mockBinaryFile.create();
+    jassert(result);
+    
+    ASSERT_TRUE(tempSource.exists());
+
+    juce::File copiedFile;
+    EXPECT_NO_THROW(
+    {
+        copiedFile = Utilities::copyPluginToTempWithHash(tempSource);
+    });
+
+    ASSERT_TRUE(copiedFile.exists());
+    EXPECT_NE(copiedFile.getFullPathName(), tempSource.getFullPathName());
+    EXPECT_EQ(copiedFile.getFileExtension(), tempSource.getFileExtension());
+}
+
+TEST(UtilitiesCopyPluginToTempWithHash, ThrowsWhenCopyFails)
+{
+    juce::File nonExistentFile("/path/to/nonexistent.dll");
+
+    EXPECT_THROW(
+    {
+        (void)Utilities::copyPluginToTempWithHash(nonExistentFile);
+    }
+    , std::runtime_error);
+}
